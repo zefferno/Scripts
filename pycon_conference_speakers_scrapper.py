47e6b6fc0e1479943b470789156c03ab0becdc07
zefferno <zefferno@gmail.com>
2017-06-15 16:58:13 +0300

    Create pycon_conference_speakers_scrapper.py

---
+++ pycon_conference_speakers_scrapper.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+PyCon speaker names and picture URL web-scrapper
+"""
+
+import re
+
+import requests
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+
+URL = "http://il.pycon.org/wwwpyconIL/speakers"
+RE_SPEAKERS = "view-speakersandsession*."
+
+if __name__ == "__main__":
+    pretty_table = PrettyTable(['Full Name', 'Picture'])
+
+    print("[+] Fetching PyCon speakers page...")
+    requests_res = requests.get(URL)
+
+    if requests_res.status_code != requests.codes.ok:
+        print(
+            "[!] Received unexpected HTTP response:  from PyCon website"
+            % requests_res.reason
+        )
+        exit(0)
+
+    soup = BeautifulSoup(requests_res.text, "html.parser")
+
+    html_elements = soup.find_all(
+        name="div", attrs={"class": re.compile(RE_SPEAKERS)}
+    )[0]
+
+    img_list = [img for img in html_elements.find_all("img")]
+
+    for img in img_list:
+        name = " ".join(img.get("alt").split())
+        picture_url = img.get("src").strip()
+        if name and picture_url:
+            pretty_table.add_row([name, picture_url])
+
+    print("[+] Printing results...")
+    print(pretty_table)
+
+    print("[+] Done")
