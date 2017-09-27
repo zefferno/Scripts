eef6dad6760be157b445367de2f2e2ebd09b4fe3
Amit Aronovitch <aronovitch@gmail.com>
2017-09-28 00:48:58 +0300

    pyconil scraper: scrape metadata, save to json

--- pycon_conference_speakers_scraper.py
+++ pycon_conference_speakers_scraper.py
@@ -2,21 +2,107 @@
 # -*- coding: utf-8 -*-
 
 """
-PyCon speakers web-scrapper
+PyCon speakers web-scraper
 """
-
-import re
+from __future__ import print_function
+import sys, re, warnings, json
 
 import requests
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 
-URL = "http://il.pycon.org/wwwpyconIL/speakers"
+URL_BASE = "http://il.pycon.org"
+URL = URL_BASE + "/wwwpyconIL/speakers"
 RE_SPEAKERS = "view-speakersandsession*."
 
-if __name__ == "__main__":
-    pretty_table = PrettyTable(['Full Name', 'Picture'])
+DATA_HANDLES = dict(
+    linkedin = 'speaker-linkin',
+    twitter = 'speaker-twitter',
+    bio = 'speaker-body',
+    sessions = 'speaker-word-session',
+)
 
+# utility funcs
+
+def _notnull(data, key):
+    return '-' if data.get(key) is None else '+'
+
+def _find_parent_div(element, class_):
+    for par in element.parents:
+        if par.name=='div' and (class_ in par.attrs.get('class')):
+            return par
+    assert False, 'no parent of class '+class_
+    
+def _find_unique(element, *args, **kwargs):
+    allow_none = kwargs.pop('allow_none', False)
+    items = element.find_all(*args, **kwargs)
+    if allow_none and len(items)==0:
+        return None
+    else:
+        assert len(items)==1
+        return items[0]
+
+def _spacify(s):
+    "convert whitespace to single space"
+    return ' '.join(s.split())
+
+def _get_link(anchor):
+    relative = anchor['href']
+    if relative.endswith('%20'):
+        relative = relative[:-3]
+    return URL_BASE + relative
+
+# extracting data from specific elements
+
+def find_sessions(head):
+    content = _find_parent_div(head, 'view-grouping-content')
+    anchors = [x.find('a') for x in content.find_all(
+        'span', class_='field-content')]
+    items = [(x.text, _get_link(x)) for x in anchors]
+    return items
+
+def find_speaker_data(head):
+    speaker = _find_parent_div(head, 'view-grouping')
+    content = _find_parent_div(head, 'view-grouping-content')
+    # several speakers might be packed together as a group with common image
+    group = _find_parent_div(content, 'view-grouping')
+    
+    data  = {}
+    data['name'] = _spacify(_find_unique(speaker, 'h1').text)
+    img_attr = _find_unique(group, 'img').attrs
+    if not img_attr['alt']:
+        assert 'default_avatar' in img_attr['src']
+        data['image'] = None
+    else:
+        if not _spacify(img_attr['alt']) == data['name']:
+            warnings.warn('error in name "{0}"!="{1}"'.format(
+                _spacify(img_attr['alt']), data['name']))
+        data['image'] = URL_BASE + img_attr['src'].split('?')[0]
+    
+    for field, cls in DATA_HANDLES.items():
+        data[field] = _find_unique(speaker, class_ = cls, allow_none=True)
+    
+    # post-processing...
+    
+    for field in ['linkedin', 'twitter']:
+        if data[field]:
+            a = data[field].find_all('a')[-1]
+            data[field] = a['href']
+    
+    data['bio'] = ''.join([str(x) for x in data['bio'].contents])
+    data['sessions'] = find_sessions(data['sessions'])
+
+    twit = data['twitter']
+    if twit and 'github' in twit:
+        data['github'] = twit
+        data['twitter'] = None
+    else:
+        data['github'] = None
+    
+    return data
+
+
+if __name__ == "__main__":
     print("[+] Fetching PyCon speakers page...")
     requests_res = requests.get(URL)
 
@@ -25,23 +111,36 @@ if __name__ == "__main__":
             "[!] Received unexpected HTTP response: %s from website"
             % requests_res
         )
-        exit(1)
-
+        sys.exit(1)
+    
     soup = BeautifulSoup(requests_res.text, "html.parser")
-
+    
+    print("[+] parsing...")
     html_elements = soup.find_all(
         name="div", attrs={"class": re.compile(RE_SPEAKERS)}
     )[0]
+    speakerlist = _find_unique(html_elements, 'div', class_='view-content')
+    db = []
+    for head in speakerlist.find_all('h1'):
+        if head.text == 'Pycon Israel Team': continue
+        db.append(find_speaker_data(head))    
+    
+    pretty_table = PrettyTable(['Full Name', 'Pic', 'In', 'twit', 'GH', 'sessions', 'bio'])
 
-    img_list = [img for img in html_elements.find_all("img")]
-
-    for img in img_list:
-        name = " ".join(img.get("alt").split())
-        picture_url = img.get("src").strip()
-        if name and picture_url:
-            pretty_table.add_row([name, picture_url])
-
+    for speaker in db:
+        pretty_table.add_row([
+            speaker['name'],
+            _notnull(speaker, 'image'), _notnull(speaker, 'linkedin'),
+            _notnull(speaker, 'twitter'), _notnull(speaker, 'github'),
+            len(speaker['sessions']), len(speaker['bio'])
+        ])
+    
     print("[+] Printing results...")
     print(pretty_table)
-
+    
+    if len(sys.argv)>1:
+        fname = sys.argv[1]
+        print("[+] writing output to file {0}...".format(fname))
+        json.dump(dict(speakers=db), open(fname, 'w'))
+    
     print("[+] Done")
